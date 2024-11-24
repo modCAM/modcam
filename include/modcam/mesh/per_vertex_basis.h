ddf6e69d186eb4f37c06dc72d6ac8196933fa12b
prosilio <prosilio.github@proton.me>
2024-11-24 09:35:27 -0700

    Convert per_vertex_basis to header-only

--- include/modcam/mesh/per_vertex_basis.h
+++ include/modcam/mesh/per_vertex_basis.h
@@ -13,22 +13,42 @@
 #ifndef PER_VERTEX_BASIS_H
 #define PER_VERTEX_BASIS_H
 
-#include "modcam_mesh_export.h"
+#include "modcam/utility/random_orthonormal.h"
 
 #include <Eigen/Core>
-
-#include <tuple>
+#include <Eigen/Geometry>
 
 namespace modcam::mesh {
 
 /**
- * Compute an orthonormal set of basis vectors where the z-axis is the vertex
- * normal.
+ * Compute an orthonormal set of basis vectors for each vertex in a mesh.
+ *
+ * Each vertex in the mesh is assigned a local coordinate system, where the
+ * z-axis is aligned with the vertex normal.
+ *
+ * @param[in] vertex_normals #V-by-3 matrix of mesh vertex 3D normals
+ * @param[out] b0 #V-by-3 matrix of basis vectors, corresponding to the
+ * vertex-local x-axis
+ * @param[out] b1 #V-by-3 matrix of basis vectors, corresponding to the
+ * vertex-local y-axis
+ * @param[out] b0 #V-by-3 matrix of basis vectors, corresponding to the
+ * vertex-local z-axis (aligned with the vertex normal)
  */
-
-MODCAM_MESH_EXPORT
-	std::tuple<Eigen::MatrixX3d, Eigen::MatrixX3d, Eigen::MatrixX3d>
-	per_vertex_basis(const Eigen::MatrixX3d &vertex_normals);
+template <typename DerivedN, typename DerivedB0, typename DerivedB1,
+          typename DerivedB2>
+void per_vertex_basis(const Eigen::MatrixBase<DerivedN> &vertex_normals,
+                      Eigen::MatrixBase<DerivedB0> &b0,
+                      Eigen::MatrixBase<DerivedB1> &b1,
+                      Eigen::MatrixBase<DerivedB2> &b2) {
+	auto num_vectors = vertex_normals.rows();
+	b2.derived().resize(num_vectors, 3);
+	b2 = vertex_normals.rowwise().normalized();
+	utility::random_orthonormal(b2, b0);
+	b1.derived().resize(num_vectors, 3);
+	for (Eigen::Index row = 0; row < num_vectors; row++) {
+		b1.row(row) = b2.row(row).cross(b0.row(row));
+	}
+}
 
 } // namespace modcam::mesh
 
--- src/modcam/mesh/per_vertex_basis.test.cpp
+++ src/modcam/mesh/per_vertex_basis.test.cpp
@@ -16,66 +16,43 @@
 #include <doctest/doctest.h>
 
 #include <cmath>
-#include <tuple>
 
 namespace modcam {
 TEST_CASE("Test per-vertex basis function") {
-	SUBCASE("Unit normal along the x-axis") {
-		// Expect z, -y, x
-		const Eigen::MatrixX3d normal_vectors{{1.0, 0.0, 0.0}};
-		std::tuple<Eigen::MatrixX3d, Eigen::MatrixX3d, Eigen::MatrixX3d> basis =
-			mesh::per_vertex_basis(normal_vectors);
-		auto &b0 = std::get<0>(basis);
-		auto &b1 = std::get<1>(basis);
-		auto &b2 = std::get<2>(basis);
+	SUBCASE("Unit normals along the coordinate axes") {
+		// Expect ( z, -y, x)
+		//        (-z, -x, y)
+		//        ( y, -x, z)
+		Eigen::MatrixX3d normal_vectors{
+			{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
+		Eigen::MatrixX3d b0;
+		Eigen::MatrixX3d b1;
+		Eigen::MatrixX3d b2;
+		mesh::per_vertex_basis(normal_vectors, b0, b1, b2);
 		CHECK((b2.array() == normal_vectors.rowwise().normalized().array())
 		          .all());
-		CHECK((b0.row(0).cross(b1.row(0)).array() == b2.row(0).array()).all());
-		CHECK((b1.row(0).cross(b2.row(0)).array() == b0.row(0).array()).all());
-	}
-	SUBCASE("Unit normal along the y-axis") {
-		// Expect -z, -x, y
-		const Eigen::MatrixX3d normal_vectors{{0.0, 1.0, 0.0}};
-		std::tuple<Eigen::MatrixX3d, Eigen::MatrixX3d, Eigen::MatrixX3d> basis =
-			mesh::per_vertex_basis(normal_vectors);
-		auto &b0 = std::get<0>(basis);
-		auto &b1 = std::get<1>(basis);
-		auto &b2 = std::get<2>(basis);
-		CHECK((b2.array() == normal_vectors.rowwise().normalized().array())
-		          .all());
-		CHECK((b0.row(0).cross(b1.row(0)).array() == b2.row(0).array()).all());
-		CHECK((b1.row(0).cross(b2.row(0)).array() == b0.row(0).array()).all());
-	}
-	SUBCASE("Unit normal along the z-axis") {
-		// Expect y, -x, z
-		const Eigen::MatrixX3d normal_vectors{{0.0, 0.0, 1.0}};
-		std::tuple<Eigen::MatrixX3d, Eigen::MatrixX3d, Eigen::MatrixX3d> basis =
-			mesh::per_vertex_basis(normal_vectors);
-		auto &b0 = std::get<0>(basis);
-		auto &b1 = std::get<1>(basis);
-		auto &b2 = std::get<2>(basis);
-		CHECK((b2.array() == normal_vectors.rowwise().normalized().array())
-		          .all());
-		CHECK((b0.row(0).cross(b1.row(0)).array() == b2.row(0).array()).all());
-		CHECK((b1.row(0).cross(b2.row(0)).array() == b0.row(0).array()).all());
+		for (Eigen::Index r = 0; r < b2.rows(); r++) {
+			CHECK((b0.row(r).cross(b1.row(r)).array() == b2.row(r).array())
+			          .all());
+			CHECK((b1.row(r).cross(b2.row(r)).array() == b0.row(r).array())
+			          .all());
+		}
 	}
 	SUBCASE("Non-unit normal vector") {
-		const Eigen::MatrixX3d normal_vectors{{1.0, 1.0, 1.0}};
-		std::tuple<Eigen::MatrixX3d, Eigen::MatrixX3d, Eigen::MatrixX3d> basis =
-			mesh::per_vertex_basis(normal_vectors);
-		auto &b0 = std::get<0>(basis);
-		auto &b1 = std::get<1>(basis);
-		auto &b2 = std::get<2>(basis);
-		CHECK((b2.array() == normal_vectors.rowwise().normalized().array())
-		          .all());
-		Eigen::MatrixX3d b0_b1 = b0.row(0).cross(b1.row(0));
-		CHECK(b0_b1(0, 0) == doctest::Approx(b2(0, 0)));
-		CHECK(b0_b1(0, 1) == doctest::Approx(b2(0, 1)));
-		CHECK(b0_b1(0, 2) == doctest::Approx(b2(0, 2)));
-		Eigen::MatrixX3d b1_b2 = b1.row(0).cross(b2.row(0));
-		CHECK(b1_b2(0, 0) == doctest::Approx(b0(0, 0)));
-		CHECK(b1_b2(0, 1) == doctest::Approx(b0(0, 1)));
-		CHECK(b1_b2(0, 2) == doctest::Approx(b0(0, 2)));
+		Eigen::RowVector3d normal_vector{{1.0, 1.0, 1.0}};
+		Eigen::RowVector3d b0;
+		Eigen::RowVector3d b1;
+		Eigen::RowVector3d b2;
+		mesh::per_vertex_basis(normal_vector, b0, b1, b2);
+		CHECK((b2.array() == normal_vector.normalized().array()).all());
+		Eigen::RowVector3d b0_x_b1 = b0.cross(b1);
+		for (Eigen::Index i = 0; i < b0_x_b1.size(); i++) {
+			CHECK(b0_x_b1(i) == doctest::Approx(b2(i)));
+		}
+		Eigen::RowVector3d b1_x_b2 = b1.cross(b2);
+		for (Eigen::Index i = 0; i < b1_x_b2.size(); i++) {
+			CHECK(b1_x_b2(i) == doctest::Approx(b0(i)));
+		}
 	}
 }
 } // namespace modcam
