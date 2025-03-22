dd0f862314d7533a3218f12dee4e8ba317015584
prosilio <prosilio.github@proton.me>
2025-03-21 21:48:32 -0600

    Switch docs to Book theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,31 +6,27 @@
 # -- Project information -------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'modCAM'
-copyright = 'contributors to the modCAM project'
+project = "modCAM"
+author = "contributors to the modCAM project"
+copyright = "contributors to the modCAM project"
 
 # -- General configuration -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-	"breathe",
-	'sphinxcontrib.bibtex'
-]
+extensions = ["breathe", "sphinxcontrib.bibtex"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
 
 # -- Breathe configuration -----------------------------------------------------
 breathe_default_project = "modCAM"
 
 # -- Bibliography configuration ------------------------------------------------
-bibtex_bibfiles = ['modCAM.bib']
-bibtex_default_style = 'unsrt'
+bibtex_bibfiles = ["modCAM.bib"]
+bibtex_default_style = "unsrt"
