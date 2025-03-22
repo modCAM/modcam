34328d56efa685606de30677d98675a5ecd79973
prosilio <prosilio.github@proton.me>
2025-03-22 15:36:37 -0600

    Switch Voronoi area to header only

--- include/modcam/mesh/voronoi_area.h
+++ include/modcam/mesh/voronoi_area.h
@@ -13,18 +13,102 @@
 #ifndef VORONOI_AREA_H
 #define VORONOI_AREA_H
 
-#include "modcam_mesh_export.h"
+#include "modcam/utility/modulus.h"
 
 #include <Eigen/Core>
+#include <Eigen/src/Core/MatrixBase.h>
+
+#include <igl/cotmatrix_entries.h>
+#include <igl/doublearea.h>
+#include <igl/edge_lengths.h>
+#include <igl/internal_angles.h>
+
+#include <numbers>
+#include <stdexcept>
 
 namespace modcam::mesh {
 
 /**
  * Compute the Voronoi cell areas for the triangles in a mesh as described in
  * @cite Meyer2003.
+ *
+ * @param[in] vertices #V-by-3 matrix of mesh vertex coordinates
+ * @param[in] faces #F-by-3 matrix of face (triangle) indices
+ * @param[out] v_area #F-by-3 matrix of the Voronoi area of each vertex in each
+ * triangle
  */
-MODCAM_MESH_EXPORT Eigen::MatrixXd voronoi_area(const Eigen::MatrixXd &vertices,
-                                                const Eigen::MatrixXi &faces);
+template <typename DerivedV, typename DerivedF, typename DerivedVA>
+void voronoi_area(const Eigen::MatrixBase<DerivedV> &vertices,
+                  const Eigen::MatrixBase<DerivedF> &faces,
+                  Eigen::MatrixBase<DerivedVA> &v_area) {
+
+	if (faces.size() == 0) {
+		v_area.derived().resize(0, 3);
+		return;
+	}
+
+	auto vertices_per_face = faces.cols();
+	if (vertices_per_face != 3) {
+		throw std::invalid_argument(
+			"There should be three vertices per face, i.e. the faces array "
+			"should have three columns.");
+	}
+
+	typedef Eigen::Matrix<typename DerivedVA::Scalar,
+	                      DerivedVA::RowsAtCompileTime, 3>
+		MatrixT;
+
+	auto num_faces = faces.rows();
+	v_area.derived().resize(num_faces, vertices_per_face);
+
+	if (vertices.size() == 0) {
+		v_area = MatrixT::Zero(num_faces, vertices_per_face);
+		return;
+	}
+
+	Eigen::ArrayXd area;
+	igl::doublearea(vertices, faces, area);
+	area /= static_cast<typename DerivedVA::Scalar>(
+		2.0); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+
+	Eigen::ArrayXXd angles;
+	igl::internal_angles(vertices, faces, angles);
+
+	Eigen::MatrixXd half_cot;
+	igl::cotmatrix_entries(vertices, faces, half_cot);
+
+	Eigen::MatrixXd edge_squared;
+	igl::edge_lengths(vertices, faces, edge_squared);
+	edge_squared = edge_squared.cwiseProduct(edge_squared);
+
+	double right_angle =
+		std::numbers::pi / 2.0; // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+	Eigen::Array<bool, Eigen::Dynamic, 1> nonobtuse =
+		(angles <= right_angle).rowwise().all();
+
+	for (Eigen::Index row = 0; row < num_faces; row++) {
+		for (Eigen::Index col = 0; col < vertices_per_face; col++) {
+			if (nonobtuse(row)) {
+				Eigen::Index i = utility::mod(col - 1, vertices_per_face);
+				Eigen::Index j = utility::mod(col + 1, vertices_per_face);
+				v_area(row, col) =
+					0.25 * // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+					(edge_squared(row, i) * half_cot(row, i) +
+				     edge_squared(row, j) * half_cot(row, j));
+			} else {
+				if (angles(row, col) > right_angle) {
+					v_area(row, col) =
+						area(row) /
+						2.0; // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+				} else {
+					v_area(row, col) =
+						area(row) /
+						4.0; // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+				}
+			}
+		}
+	}
+}
 
 } // namespace modcam::mesh
 
--- src/modcam/mesh/voronoi_area.test.cpp
+++ src/modcam/mesh/voronoi_area.test.cpp
@@ -16,15 +16,19 @@
 #include <doctest/doctest.h>
 
 #include <cmath>
+#include <numbers>
+#include <stdexcept>
 
 namespace modcam {
 TEST_CASE("Test Voronoi area function") {
 	SUBCASE("Equilateral triangle") {
-		const Eigen::MatrixX3d vertices{
-			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, std::sqrt(3.0) / 2.0, 0.0}};
+		const Eigen::MatrixX3d vertices{{0.0, 0.0, 0.0},
+		                                {1.0, 0.0, 0.0},
+		                                {0.5, std::numbers::sqrt3 / 2.0, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
-		const double one_third_area = std::sqrt(3.0) / 12.0;
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
+		const double one_third_area = std::numbers::sqrt3 / 12.0;
 		CHECK(weights(0) == doctest::Approx(one_third_area));
 		CHECK(weights(1) == doctest::Approx(one_third_area));
 		CHECK(weights(2) == doctest::Approx(one_third_area));
@@ -33,7 +37,8 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.1, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0125);
 		CHECK(weights(1) == 0.0125);
 		CHECK(weights(2) == 0.025);
@@ -41,13 +46,14 @@ TEST_CASE("Test Voronoi area function") {
 	SUBCASE("Multiple triangles") {
 		const Eigen::MatrixX3d vertices{{0.0, 0.0, 0.0},
 		                                {1.0, 0.0, 0.0},
-		                                {0.5, std::sqrt(3.0) / 2.0, 0.0},
+		                                {0.5, std::numbers::sqrt3 / 2.0, 0.0},
 		                                {0.5, 0.1, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}, {0, 1, 3}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights.rows() == faces.rows());
 		CHECK(weights.cols() == 3);
-		const double one_third_area = std::sqrt(3.0) / 12.0;
+		const double one_third_area = std::numbers::sqrt3 / 12.0;
 		CHECK(weights(0, 0) == doctest::Approx(one_third_area));
 		CHECK(weights(0, 1) == doctest::Approx(one_third_area));
 		CHECK(weights(0, 2) == doctest::Approx(one_third_area));
@@ -59,7 +65,8 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0);
 		CHECK(weights(1) == 0.0);
 		CHECK(weights(2) == 0.0);
@@ -68,7 +75,8 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.1, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 0, 0}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0);
 		CHECK(weights(1) == 0.0);
 		CHECK(weights(2) == 0.0);
@@ -77,7 +85,8 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.0, 0.0}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0);
 		CHECK(weights(1) == 0.0);
 		CHECK(weights(2) == 0.0);
@@ -86,13 +95,15 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.0, 0.0}};
 		const Eigen::MatrixXi faces(0, 0);
-		Eigen::MatrixXd weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixXd weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights.size() == 0);
 	}
 	SUBCASE("Empty vertex array") {
 		const Eigen::MatrixXd vertices(0, 0);
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixXd weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixXd weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0);
 		CHECK(weights(1) == 0.0);
 		CHECK(weights(2) == 0.0);
@@ -100,7 +111,8 @@ TEST_CASE("Test Voronoi area function") {
 	SUBCASE("2D vertex array") {
 		const Eigen::MatrixXd vertices{{0.0, 0.0}, {1.0, 0.0}, {0.5, 0.1}};
 		const Eigen::MatrixX3i faces{{0, 1, 2}};
-		Eigen::MatrixX3d weights = mesh::voronoi_area(vertices, faces);
+		Eigen::MatrixX3d weights;
+		mesh::voronoi_area(vertices, faces, weights);
 		CHECK(weights(0) == 0.0125);
 		CHECK(weights(1) == 0.0125);
 		CHECK(weights(2) == 0.025);
@@ -109,7 +121,8 @@ TEST_CASE("Test Voronoi area function") {
 		const Eigen::MatrixX3d vertices{
 			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.1, 0.0}};
 		const Eigen::MatrixXi faces{{0, 1}};
-		CHECK_THROWS_AS(mesh::voronoi_area(vertices, faces),
+		Eigen::MatrixX3d weights;
+		CHECK_THROWS_AS(mesh::voronoi_area(vertices, faces, weights),
 		                std::invalid_argument);
 	}
 }
