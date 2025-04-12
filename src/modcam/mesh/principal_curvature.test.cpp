58738ea346fe8f8d41f88930df8058af5dc3def6
prosilio <prosilio.github@proton.me>
2025-04-11 23:08:06 -0600

    Make a better icosahedron
    The icosahedron geometry seemed to be wrong, so I simplified it and
    verified that I have correct geometry.
    
    Now I need to decide if I want to run the test on a full icosahedron or
    on a subset of the faces.

--- src/modcam/mesh/principal_curvature.test.cpp
+++ src/modcam/mesh/principal_curvature.test.cpp
@@ -13,14 +13,12 @@
 #include "modcam/mesh/principal_curvature.h"
 
 #include <Eigen/Core>
-#include <Eigen/src/Core/Matrix.h>
 #include <cmrc/cmrc.hpp>
 #include <doctest/doctest.h>
 #include <igl/readSTL.h>
 #include <igl/remove_duplicate_vertices.h>
 
 #include <cmath>
-#include <cstddef>
 #include <numbers>
 #include <strstream>
 
@@ -30,119 +28,129 @@ CMRC_DECLARE(modcam);
 
 namespace modcam {
 TEST_CASE("Test principal curvature function") {
-	// SUBCASE("Sphere") {
-	// 	auto fs = cmrc::modcam::get_filesystem();
-	// 	auto sphere_path = "data/mesh/sphere.stl"s;
-	// 	REQUIRE(fs.is_file(sphere_path));
-	// 	auto sphere_rc = fs.open(sphere_path);
-	// 	std::istrstream sphere_stl(
-	// 		sphere_rc.begin(),
-	// 		sphere_rc.size()); // TODO: Change this to spanstream when I switch
-	// 	                       // to C++23.
-	// 	Eigen::MatrixX3d tmp_vertices;
-	// 	Eigen::MatrixX3i tmp_faces;
-	// 	Eigen::MatrixX3d tmp_normals;
-	// 	igl::readSTL(sphere_stl, tmp_vertices, tmp_faces, tmp_normals);
-	// 	Eigen::MatrixX3d vertices;
-	// 	Eigen::MatrixX3i faces;
-	// 	Eigen::VectorXi tmpi;
-	// 	Eigen::VectorXi tmpj;
-	// 	igl::remove_duplicate_vertices(tmp_vertices, tmp_faces, 1.0e-7,
-	// 	                               vertices, tmpi, tmpj, faces);
-	// 	Eigen::RowVector3d centers{
-	// 		(vertices.colwise().maxCoeff() - vertices.colwise().minCoeff()) /
-	// 		2.0};
-	// 	for (auto i = 0; i < 3; i++) {
-	// 		vertices.col(i).array() -=
-	// 			centers(i); // Center the sphere on the origin
-	// 	}
-	// 	vertices.rowwise().normalize(); // Set radius to 1.0
-	// 	auto radius = 11.37;
-	// 	vertices *= radius; // Make the sphere radius variable
-	// 	Eigen::MatrixX3d pd1;
-	// 	Eigen::MatrixX3d pd2;
-	// 	Eigen::VectorXd pv1;
-	// 	Eigen::VectorXd pv2;
-	// 	mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
+	SUBCASE("Sphere") {
+		auto fs = cmrc::modcam::get_filesystem();
+		auto sphere_path = "data/mesh/sphere.stl"s;
+		REQUIRE(fs.is_file(sphere_path));
+		auto sphere_rc = fs.open(sphere_path);
+		std::istrstream sphere_stl(
+			sphere_rc.begin(),
+			sphere_rc.size()); // TODO: Change this to spanstream when I switch
+		                       // to C++23.
+		Eigen::MatrixX3d tmp_vertices;
+		Eigen::MatrixX3i tmp_faces;
+		Eigen::MatrixX3d tmp_normals;
+		igl::readSTL(sphere_stl, tmp_vertices, tmp_faces, tmp_normals);
+		Eigen::MatrixX3d vertices;
+		Eigen::MatrixX3i faces;
+		Eigen::VectorXi tmpi;
+		Eigen::VectorXi tmpj;
+		igl::remove_duplicate_vertices(tmp_vertices, tmp_faces, 1.0e-7,
+		                               vertices, tmpi, tmpj, faces);
+		Eigen::RowVector3d centers{
+			(vertices.colwise().maxCoeff() - vertices.colwise().minCoeff()) /
+			2.0};
+		for (auto i = 0; i < 3; i++) {
+			vertices.col(i).array() -=
+				centers(i); // Center the sphere on the origin
+		}
+		vertices.rowwise().normalize(); // Set radius to 1.0
+		constexpr auto radius = 11.37;
+		vertices *= radius; // Make the sphere radius variable
+		Eigen::MatrixX3d pd1;
+		Eigen::MatrixX3d pd2;
+		Eigen::VectorXd pv1;
+		Eigen::VectorXd pv2;
+		mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
 
-	// 	Eigen::Index num_vertices{vertices.rows()};
-	// 	CHECK(pv1.rows() == num_vertices);
-	// 	CHECK(pv1.cols() == 1);
-	// 	CHECK(pv2.rows() == num_vertices);
-	// 	CHECK(pv2.cols() == 1);
-	// 	CHECK(pd1.rows() == num_vertices);
-	// 	CHECK(pd1.cols() == 3);
-	// 	CHECK(pd2.rows() == num_vertices);
-	// 	CHECK(pd2.cols() == 3);
+		Eigen::Index num_vertices{vertices.rows()};
+		CHECK(pv1.rows() == num_vertices);
+		CHECK(pv1.cols() == 1);
+		CHECK(pv2.rows() == num_vertices);
+		CHECK(pv2.cols() == 1);
+		CHECK(pd1.rows() == num_vertices);
+		CHECK(pd1.cols() == 3);
+		CHECK(pd2.rows() == num_vertices);
+		CHECK(pd2.cols() == 3);
 
-	// 	auto tolerance = 1.0e-6;
-	// 	CHECK(((pv1.array() - 1.0 / radius).abs() < tolerance).all());
-	// 	CHECK(((pv2.array() - 1.0 / radius).abs() < tolerance).all());
-	// }
-	// SUBCASE("Empty face array") {
-	// 	const Eigen::MatrixX3d vertices{
-	// 		{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.0, 0.0}};
-	// 	const Eigen::MatrixX3i faces(0, 3);
-	// 	Eigen::MatrixX3d pd1;
-	// 	Eigen::MatrixX3d pd2;
-	// 	Eigen::VectorXd pv1;
-	// 	Eigen::VectorXd pv2;
-	// 	mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
-	// 	CHECK(pv1.size() == 0);
-	// 	CHECK(pv2.size() == 0);
-	// 	CHECK(pd1.size() == 0);
-	// 	CHECK(pd2.size() == 0);
-	// }
-	// SUBCASE("Empty vertex array") {
-	// 	const Eigen::MatrixX3d vertices(0, 3);
-	// 	const Eigen::MatrixX3i faces{{0, 1, 2}};
-	// 	Eigen::MatrixX3d pd1;
-	// 	Eigen::MatrixX3d pd2;
-	// 	Eigen::VectorXd pv1;
-	// 	Eigen::VectorXd pv2;
-	// 	mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
-	// 	CHECK(pv1.size() == 0);
-	// 	CHECK(pv2.size() == 0);
-	// 	CHECK(pd1.size() == 0);
-	// 	CHECK(pd2.size() == 0);
-	// }
-	// SUBCASE("Icosahedron") {
-	// 	constexpr auto phi = std::numbers::phi;
-	// 	Eigen::MatrixX3d vertices{
-	// 		{phi, 1.0, 0.0},   {phi, -1.0, 0.0},  {-phi, -1.0, 0.0},
-	// 		{-phi, 1.0, 0.0},  {1.0, 0.0, phi},   {-1.0, 0.0, phi},
-	// 		{-1.0, 0.0, -phi}, {1.0, 0.0, -phi},  {0.0, phi, 1.0},
-	// 		{0.0, phi, -1.0},  {0.0, -phi, -1.0}, {0.0, -phi, 1.0}};
-	// 	vertices.rowwise().normalize(); // Set radius to 1.0
-	// 	Eigen::MatrixX3i faces{{5, 4, 8},  {4, 5, 11},  {8, 4, 0},   {4, 1, 0},
-	// 	                       {4, 11, 1}, {11, 10, 1}, {11, 2, 10}, {5, 2, 11},
-	// 	                       {1, 10, 7}, {0, 1, 7},   {5, 8, 3},   {2, 5, 3},
-	// 	                       {0, 7, 9},  {10, 2, 6}};
-	// 	Eigen::MatrixX3d pd1;
-	// 	Eigen::MatrixX3d pd2;
-	// 	Eigen::VectorXd pv1;
-	// 	Eigen::VectorXd pv2;
-	// 	mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
-	// 	Eigen::Index num_vertices{vertices.rows()};
-	// 	CHECK(pv1.rows() == num_vertices);
-	// 	CHECK(pv1.cols() == 1);
-	// 	CHECK(pv2.rows() == num_vertices);
-	// 	CHECK(pv2.cols() == 1);
-	// 	CHECK(pd1.rows() == num_vertices);
-	// 	CHECK(pd1.cols() == 2);
-	// 	CHECK(pd2.rows() == num_vertices);
-	// 	CHECK(pd2.cols() == 2);
-	// 	const double radius = 1.0;
-	// 	int inner_vertices[3] = {1, 4, 11};
-	// 	for (int i = 0; i < 3; i++) {
-	// 		CHECK(pv1(inner_vertices[i]) == doctest::Approx(1.0 / radius));
-	// 		CHECK(pv2(inner_vertices[i]) == doctest::Approx(1.0 / radius));
-	// 	}
-	// 	int excluded_vertices[3] = {3, 6, 9};
-	// 	for (int i = 0; i < 3; i++) {
-	// 		CHECK(pd1.row(excluded_vertices[i]).array().isNaN().all());
-	// 	}
-	// }
+		auto tolerance = 1.0e-6;
+		CHECK(((pv1.array() - 1.0 / radius).abs() < tolerance).all());
+		CHECK(((pv2.array() - 1.0 / radius).abs() < tolerance).all());
+	}
+	SUBCASE("Empty face array") {
+		const Eigen::MatrixX3d vertices{
+			{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.0, 0.0}};
+		const Eigen::MatrixX3i faces(0, 3);
+		Eigen::MatrixX3d pd1;
+		Eigen::MatrixX3d pd2;
+		Eigen::VectorXd pv1;
+		Eigen::VectorXd pv2;
+		mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
+		CHECK(pv1.size() == 0);
+		CHECK(pv2.size() == 0);
+		CHECK(pd1.size() == 0);
+		CHECK(pd2.size() == 0);
+	}
+	SUBCASE("Empty vertex array") {
+		const Eigen::MatrixX3d vertices(0, 3);
+		const Eigen::MatrixX3i faces{{0, 1, 2}};
+		Eigen::MatrixX3d pd1;
+		Eigen::MatrixX3d pd2;
+		Eigen::VectorXd pv1;
+		Eigen::VectorXd pv2;
+		mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
+		CHECK(pv1.size() == 0);
+		CHECK(pv2.size() == 0);
+		CHECK(pd1.size() == 0);
+		CHECK(pd2.size() == 0);
+	}
+	SUBCASE("Icosahedron") {
+		constexpr auto pi = std::numbers::pi;
+		const auto r = 1.0 / (2.0 * std::sin(pi / 5.0));
+		using Array12d = Eigen::Array<double, 12, 1>;
+		using Array5d = Eigen::Array<double, 5, 1>;
+		Array12d phi = Array12d::Zero();
+		phi(Eigen::seq(1, 5)) = Array5d::Constant(std::asin(r));
+		phi(Eigen::seq(6, 10)) = Array5d::Constant(-std::asin(r));
+		phi(11) = pi;
+		Array12d theta = Array12d::Zero();
+		theta(Eigen::seq(1, 5)) = Array5d::LinSpaced(5, 0.0, 8.0 * pi / 5.0);
+		theta(Eigen::seq(6, 10)) =
+			Array5d::LinSpaced(5, pi / 5.0, 9.0 * pi / 5.0);
+		Eigen::MatrixX3d vertices = Eigen::MatrixX3d::Zero(12, 3);
+		vertices.col(0) = Eigen::sin(phi) * Eigen::cos(theta);
+		vertices.col(1) = Eigen::sin(phi) * Eigen::sin(theta);
+		vertices.col(2) = Eigen::cos(phi);
+		// vertices.rowwise().normalize(); // Set radius to 1.0
+		constexpr auto radius = 1.0;
+		vertices *= radius;
+		const Eigen::MatrixX3i faces{
+			{0, 1, 2},  {0, 2, 3},  {0, 3, 4},  {0, 4, 5},   {0, 5, 1},
+			{2, 1, 6},  {3, 2, 7},  {4, 3, 8},  {5, 4, 9},   {1, 5, 10},
+			{6, 7, 2},  {7, 8, 3},  {8, 9, 4},  {9, 10, 5},  {10, 6, 1},
+			{11, 7, 6}, {11, 8, 7}, {11, 9, 8}, {11, 10, 9}, {11, 6, 10}};
+		Eigen::MatrixX3d pd1;
+		Eigen::MatrixX3d pd2;
+		Eigen::VectorXd pv1;
+		Eigen::VectorXd pv2;
+		mesh::principal_curvature_rus2004(vertices, faces, pd1, pd2, pv1, pv2);
+		Eigen::Index num_vertices{vertices.rows()};
+		CHECK(pv1.rows() == num_vertices);
+		CHECK(pv1.cols() == 1);
+		CHECK(pv2.rows() == num_vertices);
+		CHECK(pv2.cols() == 1);
+		CHECK(pd1.rows() == num_vertices);
+		CHECK(pd1.cols() == 3);
+		CHECK(pd2.rows() == num_vertices);
+		CHECK(pd2.cols() == 3);
+		for (auto inner_vertices : {1, 4, 11}) {
+			CHECK(pv1(inner_vertices) == doctest::Approx(1.0 / radius));
+			CHECK(pv2(inner_vertices) == doctest::Approx(1.0 / radius));
+		}
+		for (auto excluded_vertices : {3, 6, 9}) {
+			CHECK(pd1.row(excluded_vertices).array().isNaN().all());
+		}
+	}
 	SUBCASE("Cylinder") {
 		const double radius = 0.5;
 		const double x0 = radius;
