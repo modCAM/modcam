6ce19a902820c156ffff093aae5102a4780dbf34
prosilio <prosilio.github@proton.me>
2025-08-16 22:11:56 -0600

    Add concepts documentation

--- docs/reference/mesh.rst
+++ docs/reference/mesh.rst
@@ -1,26 +1,41 @@
-**************
-Mesh functions
-**************
+****
+Mesh
+****
+
+Namespace
+=========
 
 .. doxygennamespace:: modcam::mesh
    :desc-only:
 
+Concepts
+========
+
+.. doxygenconcept:: modcam::mesh::TriangleFaces
+
+.. doxygenconcept:: modcam::mesh::Vectors3D
+
+.. doxygenconcept:: modcam::mesh::Vertices3D
+
+Functions
+=========
+
 per_vertex_basis
-================
+----------------
 
 .. doxygenfunction:: modcam::mesh::per_vertex_basis
 
 per_vertex_normals
-==================
+------------------
 
 .. doxygenfunction:: modcam::mesh::per_vertex_normals
 
 principal_curvature_rus2004
-===========================
+---------------------------
 
 .. doxygenfunction:: modcam::mesh::principal_curvature_rus2004
 
 voronoi_area
-============
+------------
 
 .. doxygenfunction:: modcam::mesh::voronoi_area
--- include/modcam/mesh/concepts.h
+++ include/modcam/mesh/concepts.h
@@ -19,20 +19,35 @@
 
 namespace modcam::mesh {
 
+/**
+ * An array in which each row represents a triangle by indexing into an array of
+ * vertices. For example, [3, 1, 7] represents the triangle formed by the
+ * vertices in (zero-indexed) rows 3, 1, and 7 of a vertex coordinates array.
+ */
+template <typename F>
+concept TriangleFaces =
+	std::integral<typename F::Scalar> &&
+	(F::ColsAtCompileTime == 3 || F::ColsAtCompileTime == Eigen::Dynamic);
+
+/**
+ * An array in which each row represents a three-dimensional vector. The first
+ * column is the x-coordinate, the second column is the y-coordinate, and the
+ * third column is the z-coordinate.
+ */
 template <typename V>
 concept Vectors3D =
 	std::floating_point<typename V::Scalar> &&
 	(V::ColsAtCompileTime == 3 || V::ColsAtCompileTime == Eigen::Dynamic);
 
+/**
+ * An array in which each row contains the three-dimensional coordinates for a
+ * vertex. The first column is the x-coordinate, the second column is the
+ * y-coordinate, and the third column is the z-coordinate.
+ */
 template <typename V>
 concept Vertices3D =
 	std::floating_point<typename V::Scalar> &&
 	(V::ColsAtCompileTime == 3 || V::ColsAtCompileTime == Eigen::Dynamic);
-
-template <typename F>
-concept TriangleFaces =
-	std::integral<typename F::Scalar> &&
-	(F::ColsAtCompileTime == 3 || F::ColsAtCompileTime == Eigen::Dynamic);
 
 } // namespace modcam::mesh
 
